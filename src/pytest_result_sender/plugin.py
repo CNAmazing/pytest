286673f6387647cd0a2f87b33dd485b0d7e1031b
Amazing <1569678894@qq.com>
2024-11-02 17:46:45 +0800

    增添配置文件pytest.ini plugin.py增加函数pytest_addoption send_result

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -9,6 +9,17 @@ data = {
     "failed": 0,
 }
 
+def pytest_addoption(parser):
+    parser.addini(
+        "send_when",
+        help="什么时候发送测试结果"
+    )
+    parser.addini(
+        "send_api",
+        help="结果发往何处"
+    )
+
+
 
 def pytest_runtest_logreport(report: pytest.TestReport):
     if report.when == "call":
@@ -21,10 +32,11 @@ def pytest_collection_finish(session: pytest.Session):
     print(f"用例的总数为：{data['total']}")
 
 
-def pytest_configure():
+def pytest_configure(config: pytest.Config):
     # 配置加载后 测试用例执行前执行
     data["start_time"] = datetime.now()
-
+    data["send_when"]=config.getini("send_when")
+    data["send_api"]=config.getini("send_api")
 
 def pytest_unconfigure():
     # 配置加载后 测试用例执行前执行
@@ -37,7 +49,16 @@ def pytest_unconfigure():
     # assert data["passed"]==2
     # assert data["failed"]==1
     # assert data["pass_ratio"]=="66.67%"
-    print(data["duration"])
+    send_result()
+
+def send_result():
+
+    if data["send_when"]=="on_failed" and data["failed"]==0:
+        return
+
+    if not data["send_when"]:
+        return
+
     content = f"""
     pytest集成测试
      
@@ -50,5 +71,8 @@ def pytest_unconfigure():
      
     测试报告地址： https://www.openai.com
     """
-    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=4903e472-66c3-4342-b70d-f5a7e7c5fef6"
-    requests.post(url, json={"msgtype": "markdown", "markdown": {"content": content}})
+    url = data['send_api']
+    try:
+        requests.post(url, json={"msgtype": "markdown", "markdown": {"content": content}})
+    except Exception :
+        pass
