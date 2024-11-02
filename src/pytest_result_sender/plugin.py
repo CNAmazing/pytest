ae021107689ca333f57ff693c74e11f4136a4aaf
Amazing <1569678894@qq.com>
2024-11-02 13:11:24 +0800

    配置 black isort flake8

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -1,9 +1,13 @@
-from datetime import  datetime
+from datetime import datetime
+
+print("测试重置")
 
 def pytest_configure():
-    #配置加载后 测试用例执行前执行
+    # 配置加载后 测试用例执行前执行
     print(f"{datetime.now()}pytest开始执行")
 
+
 def pytest_unconfigure():
-    #配置加载后 测试用例执行前执行
+    # 配置加载后 测试用例执行前执行
+
     print(f"{datetime.now()}pytest结束执行")
